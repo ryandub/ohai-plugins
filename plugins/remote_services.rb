6b882ba0324d9e0035ff80b2f0cff21a9224f8b0
Ziad Sawalha <gerrit@highbridgellc.com>
2014-01-02 13:46:57 -0600

    remote services: write as array + include path

--- plugins/remote_services.rb
+++ plugins/remote_services.rb
@@ -5,16 +5,17 @@ if output
   lines = output.split(/\n/).reject(&:empty?)[2..-1]
 end
 
-remote_services Mash.new
+remote_services Array.new
 if lines
   for line in lines
     line_data = line.split()
-    remote_services[line_data[3]] = {
+    remote_services.push([line_data[3]] = {
       :protocol => line_data[0],
       :ip => line_data[3].split(":")[0],
       :port => line_data[3].split(":")[-1],
       :pid => line_data[-1].split("/")[0],
-      :process => line_data[-1].split("/")[1]
+      :process => line_data[-1].split("/")[1],
+      :path => line_data[-1]
     }
   end
 end
