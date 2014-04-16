4c16e2a976f1f9eeae34c61a4036bfd63a1c84fb
Hart Hoover <hart.hoover@rackspace.com>
2014-04-16 15:34:51 -0500

    Add testing for cron plugin

---
+++ test/integration/ohaiplugins/serverspec/localhost/cron_spec.rb
@@ -0,0 +1,35 @@
+# Encoding: utf-8
+require 'spec_helper'
+
+cronjobs = OHAI['cronjobs']
+
+describe 'Cron Plugin' do
+
+	it 'should be an array' do
+    	expect(cronjobs).to be_a(Mash)
+  	end
+
+  	it 'should report the minute' do
+  		expect(cronjobs['m']).to eql('0')
+  	end
+
+  	it 'should report the hour' do
+  		expect(cronjobs['h']).to eql('0')
+  	end
+
+  	it 'should report the day of the month' do
+  		expect(cronjobs['dom']).to eql('1')
+  	end
+
+  	it 'should report the month' do
+  		expect(cronjobs['mon']).to eql('3')
+  	end
+
+	it 'should report the day of the week' do
+  		expect(cronjobs['dow']).to eql('0')
+  	end
+
+	it 'should report the command' do
+  		expect(cronjobs['command']).to include('echo')
+  	end
+end
