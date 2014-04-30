44d01ae63a10dc5fe74d8f42b8e0e87ea6d695a7
Ryan Walker <ryan.walker@rackspace.com>
2014-04-30 16:37:27 -0500

    Fix sshd test to skip cent/rhel 5

--- test/integration/ohaiplugins/serverspec/localhost/sshd_spec.rb
+++ test/integration/ohaiplugins/serverspec/localhost/sshd_spec.rb
@@ -2,35 +2,41 @@
 require 'spec_helper'
 
 sshd = OHAI['sshd']
+platform_family = OHAI['platform_family']
+platform_version = OHAI['platform_version'].to_f
 
-describe 'sshd Plugin' do
+unless platform_family == 'rhel' && platform_version < 6
 
-  it 'should be a Mash' do
-    expect(sshd).to be_a(Mash)
-  end
+  describe 'sshd Plugin' do
 
-  it 'should have a value' do
-    expect(sshd.keys).not_to be_empty
-  end
+    it 'should be a Mash' do
+      expect(sshd).to be_a(Mash)
+    end
 
-  it 'should have port value of 22' do
-    expect(sshd['port']).to eql('22')
-  end
+    it 'should have a value' do
+      expect(sshd.keys).not_to be_empty
+    end
 
-  it 'should use ssh protocol 2' do
-    expect(sshd['protocol']).to eql('2')
-  end
+    it 'should have port value of 22' do
+      expect(sshd['port']).to eql('22')
+    end
 
-  it 'should have permitrootlogin value of "yes"' do
-    expect(sshd['permitrootlogin']).to eql('yes')
-  end
+    it 'should use ssh protocol 2' do
+      expect(sshd['protocol']).to eql('2')
+    end
 
-  it 'should have x11forwarding value of "yes"' do
-    expect(sshd['x11forwarding']).to eql('yes')
-  end
+    it 'should have permitrootlogin value of "yes"' do
+      expect(sshd['permitrootlogin']).to eql('yes')
+    end
 
-  it 'should have ignorerhosts value of "yes"' do
-    expect(sshd['ignorerhosts']).to eql('yes')
+    it 'should have x11forwarding value of "yes"' do
+      expect(sshd['x11forwarding']).to eql('yes')
+    end
+
+    it 'should have ignorerhosts value of "yes"' do
+      expect(sshd['ignorerhosts']).to eql('yes')
+    end
+
   end
 
 end
