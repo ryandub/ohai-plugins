1dbb09691e3ee1d3a490bbb692bfe4e2761df384
Hart Hoover <hart.hoover@rackspace.com>
2014-04-30 12:02:40 -0500

    Parse iptables rules into a dict

--- plugins/iptables.rb
+++ plugins/iptables.rb
@@ -1,5 +1,5 @@
 # Encoding: utf-8
-# A plugin to discover IPTables rules configured on a server
+# A plugin to discover IPtables rules configured on a server
 Ohai.plugin(:Iptables) do
 	provides 'iptables'
 
@@ -7,7 +7,26 @@ Ohai.plugin(:Iptables) do
 		iptables Mash.new # Creating a mash of present IPTables rules
 		so = shell_out("sudo iptables -S")
 		so.stdout.each_line.with_index do |line, i|
-			iptables[i] = line.strip
+			case line
+			when /^-P/
+				fields = line.strip.split(" ")
+				iptables[i] = {
+					"type" => "policy",
+					"chain" => fields[1],
+					"policy" => fields[2]
+				}
+			when /^-A/
+				dict = {}
+				clobbered_fields = line.split("-")
+				fields = clobbered_fields.map { |field| field.split(" ")}
+				fields.each do |field| 
+					key = field[0] == "A" ? "chain" : field[0].to_s 
+					dict[key] = field[1]
+				end
+				dict.delete("")
+				dict["type"] = "rule"
+				iptables[i] = dict
+			end
 		end
 	end
 end
