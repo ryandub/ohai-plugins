4cb86ff726c96642ed23558e4d0eadcc38c5344c
Ryan Walker <ryan.walker@rackspace.com>
2014-04-30 10:11:11 -0500

    Accidentially the whole debug line...

--- plugins/sshd.rb
+++ plugins/sshd.rb
@@ -5,7 +5,6 @@ Ohai.plugin(:Sshd) do
   collect_data(:linux) do
     sshd Mash.new
     so = shell_out('sshd -T')
-    Ohai::Log.debug(so.stdout)
     so.stdout.lines do |line|
       line = line.split(' ')
       sshd[line[0]] = line[1..-1].join(' ')
