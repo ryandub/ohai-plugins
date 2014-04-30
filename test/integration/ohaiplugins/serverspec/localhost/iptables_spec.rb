977ab7de9fdc7710819fb20a081b48cae9665337
Hart Hoover <hart.hoover@rackspace.com>
2014-04-30 12:33:58 -0500

    Indent expect lines, for real

--- test/integration/ohaiplugins/serverspec/localhost/iptables_spec.rb
+++ test/integration/ohaiplugins/serverspec/localhost/iptables_spec.rb
@@ -4,13 +4,13 @@ iptables = OHAI['iptables']
 
 describe 'Iptables Plugin' do
 	
-	it 'should be an Mash' do
-      expect(iptables).to be_a(Mash)
-  	end
+  it 'should be an Mash' do
+    expect(iptables).to be_a(Mash)
+  end
 
-  	it 'should contain rules' do
-      expect(iptables[0]).not_to be_empty
-  	end
+  it 'should contain rules' do
+    expect(iptables[0]).not_to be_empty
+  end
 
 end
 
