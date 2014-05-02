cc14ad1febfc4017adac51478579edc0bb918dea
BK Box <bk.box@rackspace.com>
2014-05-02 14:20:17 -0500

    Add some stats around fail2ban

--- plugins/fail2ban.rb
+++ plugins/fail2ban.rb
@@ -1,50 +1,72 @@
 Ohai.plugin(:Fail2ban) do
   provides 'fail2ban'
 
-  def get_logfile()
+  def logfile_path
     so = shell_out('fail2ban-client get logtarget')
     logline = so.stdout.split("\n")[1]
     loglocation = logline.split(' ')[1]
     Ohai::Log.debug("fail2ban log location: #{loglocation}")
 
     if loglocation == 'SYSLOG'
-      return '/var/log/messages' if File.exist?('/var/log/messages')
-
-      return '/var/log/syslog'
+      loglocation = '/var/log/syslog'
+      loglocation = '/var/log/messages' if File.exist?('/var/log/messages')
     end
 
-    return loglocation
+    loglocation
   end
 
-  def get_jails()
+  def jails_list
     so = shell_out('fail2ban-client status')
     lines = so.stdout.split("\n")
     Ohai::Log.debug("fail2ban status: #{lines}")
     comma_jails = lines[2].split("\t\t")[1]
-    list_of_jails = [*comma_jails.split(", ")]
+    list_of_jails = [*comma_jails.split(', ')]
     Ohai::Log.debug("fail2ban jails: #{list_of_jails}")
-    return list_of_jails
+    list_of_jails
   end
 
-  def get_activity()
-    logfile = get_logfile()
+  def activity_log
+    logfile = logfile_path
 
     so = shell_out("grep -e 'Ban\\|Unban' #{logfile}")
     lines = so.stdout.split("\n")
     Ohai::Log.debug("fail2ban ban lines: #{lines}")
 
-    return lines
+    lines
+  end
+
+  def banned_stats(activity)
+    ban_stats = {}
+    activity.each do |line|
+      words = line.split(' ')
+      ip = words[-1]
+      status = words[-2]
+      jail = words[-3].tr('][', '')
+
+      if ban_stats[ip] && ban_stats[ip][jail]
+        ban = ban_stats[ip][jail]
+      else
+        ban = { count: 0, status: 'None' }
+      end
+      ban[:count] += 1 if line.include? 'Ban'
+      ban[:status] = status
+      ban_stats[ip] = {} unless ban_stats[ip]
+      ban_stats[ip][jail] = ban
+    end
+    Ohai::Log.debug("Ban Stats: #{ban_stats}")
+    ban_stats
   end
 
   collect_data(:linux) do
-    activity = get_activity()
-    jails = get_jails()
+    activity = activity_log
+    jails = jails_list
+    banned = banned_stats(activity)
 
-    unless activity.empty? and jails.empty?
-      fail2ban Mash.new
-    end
-
-    fail2ban[:activity] = activity unless activity.empty?
+    fail2ban Mash.new unless activity.empty? && jails.empty?
+    Ohai::Log.debug(activity[-500..-1] || activity)
+    fail2ban[:activity] = (activity[-500..-1] || activity) unless activity.empty?
     fail2ban[:jails] = jails unless jails.empty?
+    fail2ban[:banned] = banned unless banned.empty?
+    Ohai::Log.debug("Final fail2ban data: #{fail2ban}")
   end
 end
--- test/integration/ohaiplugins/serverspec/localhost/fail2ban_spec.rb
+++ test/integration/ohaiplugins/serverspec/localhost/fail2ban_spec.rb
@@ -8,7 +8,16 @@ describe 'fail2ban Plugin' do
   end
 
   it 'should find lines in the fail2ban log' do
-    expect(fail2ban['activity'][0]).to eql('2014-04-30 10:46:24,006 fail2ban.actions: WARNING [ssh] Ban 1.1.1.1')
+    line='2014-04-30 10:46:24,006 fail2ban.actions: WARNING [ssh] Ban 1.1.1.1'
+    expect(fail2ban['activity'][0]).to eql(line)
+  end
+
+  it 'should find one ban on 1.1.1.1' do
+    expect(fail2ban['banned']['1.1.1.1']['ssh']['count']).to eql(1)
+  end
+
+  it 'should find 2.2.2.2 banned' do
+    expect(fail2ban['banned']['2.2.2.2']['ssh']['status']).to eql('Ban')
   end
 
   it 'should find active jails' do
