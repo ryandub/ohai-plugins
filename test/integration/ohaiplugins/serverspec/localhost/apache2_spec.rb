396b2969d3eabe5c06d2c71cb16593795b27f533
autotune <root@ohaiplugins-ubuntu-1404.vagrantup.com>
2014-06-20 17:44:26 +0000

    MaxClients check, test, and spec_helper v7 update

--- test/integration/ohaiplugins/serverspec/localhost/apache2_spec.rb
+++ test/integration/ohaiplugins/serverspec/localhost/apache2_spec.rb
@@ -15,7 +15,7 @@ describe "Apache2 Plugin" do
     apache_bin = '/usr/sbin/apache2'
     apache_config_path = '/etc/apache2'
     apache_config_file = '/etc/apache2/apache2.conf'
-    apache_clients_status = 'MaxClients Reached'
+    apache_clients_status = 'MaxClients OK'
     if platform == 'ubuntu' and platform_version >= 13.10
       apache_mpm = 'prefork'
       if platform_version >= 14.04
@@ -32,7 +32,7 @@ describe "Apache2 Plugin" do
     apache_config_path = '/etc/httpd'
     apache_config_file = '/etc/httpd/conf/httpd.conf'
     apache_mpm = 'prefork'
-    apache_clients_status = 'MaxClients Reached'
+    apache_clients_status = 'MaxClients OK'
   end
 
   it 'should return "MaxClients OK"' do
