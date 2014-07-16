5c7de2988014f4db76cc75d63545349a76d9bed8
marc4926 <marcin.stangel@rackspace.com>
2014-07-16 10:50:05 +0100

    Added changes suggested by Ryan, as well as discovering a little bug (when postfix is installed but not running, the plugin would not handle that and would crash, due to the find_process() method thrying to do a split on an empty string... ). Now when postfix is installed and not running, the plugin returns: Master Process: Postfix is not running!

--- plugins/postfix.rb
+++ plugins/postfix.rb
@@ -2,22 +2,26 @@ Ohai.plugin(:Postfix) do
  provides 'postfix'
  
  depends 'packages'
- depends 'processes'
+
  
  def find_postfix_executable
   so = shell_out("/bin/bash -c 'command -v postfix'")
   postfix_bin = so.stdout.strip
-  return postfix_bin
+  return postfix_bin unless postfix_bin.empty?
  end
 
   def find_postfix_process
    unless @postfix_process
     postfix_process = {}
     so = shell_out("ps aux | grep [m]aster | awk '{print $2 \", \"$11}' ")
-    ps_output = so.stdout.split(', ')
-    postfix_process = { "Master Process PID" => ps_output[0].chomp,
+     if !so.stdout.empty?
+      ps_output = so.stdout.split(', ')
+      postfix_process = { "Master Process PID" => ps_output[0].chomp,
                         "Master Process" => ps_output[1].chomp
-                      }
+                        }
+     else
+      postfix_process = {"Master Process" => 'Postfix is not running!'}
+    end
    end
    return postfix_process
   end
@@ -56,10 +60,12 @@ Ohai.plugin(:Postfix) do
     
   
   collect_data(:linux) do
-    postfix Mash.new
-    postfix[:postfix_binary] = find_postfix_executable
-    postfix[:postfix_package] = packages[:postfix]
-    postfix[:process] = find_postfix_process
-    postfix[:current_configuration] = check_postfix_configuration
+    if postfix_binary = find_postfix_executable
+     postfix Mash.new
+     postfix[:postfix_binary] = postfix_binary
+     postfix[:postfix_package] = packages[:postfix]
+     postfix[:process] = find_postfix_process
+     postfix[:current_configuration] = check_postfix_configuration
+    end  
   end
 end
