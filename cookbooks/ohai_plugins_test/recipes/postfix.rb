410375f45931643acb87f081be321a548851e163
Ryan Walker <ryan.walker@rackspace.com>
2014-07-21 17:48:37 -0500

    More postfix test updates

--- cookbooks/ohai_plugins_test/recipes/postfix.rb
+++ cookbooks/ohai_plugins_test/recipes/postfix.rb
@@ -1,1 +1,19 @@
 package 'postfix'
+
+case node['platform_family']
+when 'rhel', 'fedora'
+  service 'sendmail' do
+    action :nothing
+  end
+
+  execute 'set_postfix_to_default' do
+    command '/usr/sbin/alternatives --set mta /usr/sbin/sendmail.postfix'
+    notifies :stop, 'service[sendmail]'
+    notifies :start, 'service[postfix]'
+    not_if '/usr/bin/test /etc/alternatives/mta -ef /usr/sbin/sendmail.postfix'
+  end
+end
+
+service 'postfix' do
+  action :start
+end
