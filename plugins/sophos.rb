b6e285b07e0bb754be54266bfc7ddfa563a63a39
Cian Brennan <cian.brennan@rackspace.co.uk>
2014-08-01 18:16:02 +0100

    finished work on sophos plugin, to return last update date and version

--- plugins/sophos.rb
+++ plugins/sophos.rb
@@ -1,5 +1,6 @@
 # Encoding utf-8
 Ohai.plugin(:Sophos) do
+  require 'date'
   provides 'sophos'
 
   def sophos_bin()
@@ -12,14 +13,30 @@ Ohai.plugin(:Sophos) do
     sophos Mash.new
     so = shell_out("/opt/sophos-av/bin/savdstatus --version")
     so.stdout.lines do |line|
-      version = /Sophos Anti-Virus[[:space:]]+=[[:space:]]([[[:digit:]].]+)/.match(line)
+      version = /Sophos Anti-Virus[[:space:]]+=[[:space:]]+([0-9.]+)/.match(line)
         if version
-          sophos['version'] = version[1]
+          sophos[:version] = version[1]
           next
         end
-      last_update = /Last update[[:space:]]+=[[:space:]](.*)/.match(line)
+      last_update = /Last update[[:space:]]+=[[:space:]]+(.*)/.match(line)
       if last_update
-        sophos['update'] = Date.strptime(last_update[1], format='%a %d %b %Y %r %Z')
+
+        datetime = /[[:alpha:]]{3} ([[:alpha:]]{3})[[:space:]]{1,2}([[:digit:]]{1,2}) ([[:digit:]]{2}):([[:digit:]]{2}):([[:digit:]]{2}) ([[:digit:]]{4})/.match(last_update[1])
+
+        month = Date::ABBR_MONTHNAMES.index(datetime[1])
+        day = datetime[2].to_i
+        hour = datetime[3].to_i
+        minute = datetime[4].to_i
+        second = datetime[5].to_i
+        year = datetime[6].to_i
+        update_date = Mash.new
+        update_date[:time] = "%2d:%2d:%2d" % [ hour, minute, second ]
+        update_date[:date] = {
+          'day' => "%2d" % day,
+          'month' => "%2d" % month,
+          'year' => "%4d" % year
+        }
+        sophos[:last_update] = update_date
       end
     end
 
