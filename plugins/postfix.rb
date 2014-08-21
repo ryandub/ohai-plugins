5a1abb35418e4f4e2606014572c7cc9315447285
marc4926 <marcin.stangel@rackspace.com>
2014-08-21 17:19:14 +0100

    Added a clever way of checking the size of different postifx queues, qithout causing impact on servers that have large queues !

--- plugins/postfix.rb
+++ plugins/postfix.rb
@@ -39,7 +39,8 @@ Ohai.plugin(:Postfix) do
       "mynetworks" => "Postfix Trusted Client Networks",
       "myorigin" => "Postfix Origin Address",
       "alias_database" => "Postfix Aliases Database",
-      "config_directory" => "Postfix Configuration Directory"
+      "config_directory" => "Postfix Configuration Directory",
+      "queue_directory" => "Postfix Queue Directory"
     }
       
     postconfOutput = shell_out('postconf')
@@ -58,7 +59,43 @@ Ohai.plugin(:Postfix) do
    
     return response
   end  
+  
+  # check the size of mail queue without impact (without running mailq command)
+  
+  def get_file_paths(path)
+    Dir.glob(path + '/**/*').each do |f|
+    yield f
+   end
+  end
+  
+  def check_mailq_size
+    response = {}
+    mail_queue_output = shell_out('postconf -h queue_directory')
+    mail_queue_directory = mail_queue_output.stdout.strip
+    queueKeys = {
+          "incoming" => "Incoming Mail",
+          "active" => "Active Mail",
+          "deferred" => "Deferred Mail",
+          "bounce" => "Bounced Mail",
+          "hold" => "Hold Mail",
+          "corrupt" => "Corrupt Mail"
+        }  
+    postfix_queues = {'incoming' => 0,'active' => 0,'deferred' => 0,'bounce' => 0,'hold' => 0,'corrupt' => 0}
+    temporary_array = []
+    postfix_queues.each do |queue, size|
+      # without the extra / it does not work!
+      path = mail_queue_directory + '/' + queue
+      files = get_file_paths(path) {|f| temporary_array << f }
+      postfix_queues[queue] = files.length
+    end
 
+    queueKeys.each do |key, value|
+      response[value] = postfix_queues[key]
+    end
+    
+    
+    return response
+  end
 
   collect_data(:linux) do
     if postfix_binary = find_postfix_executable
@@ -67,6 +104,7 @@ Ohai.plugin(:Postfix) do
       postfix[:postfix_package] = packages[:postfix]
       postfix[:process] = find_postfix_process
       postfix[:current_configuration] = check_postfix_configuration
+      postfix[:mail_queue_sizes] = check_mailq_size
     end  
   end
 end
