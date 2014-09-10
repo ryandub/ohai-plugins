a2b82623ad70c9bf8fe2322b6f7e303272051782
Ryan Walker <ryan.walker@rackspace.com>
2014-09-10 13:33:17 -0500

    Update tests for Magento

--- test/integration/ohaiplugins/serverspec/localhost/magento_spec.rb
+++ test/integration/ohaiplugins/serverspec/localhost/magento_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 magento = OHAI['webapps']['magento']
 
-describe "Magento Plugin" do
+describe 'Magento Plugin' do
   it 'should be a hash' do
     expect(magento['magento.example.com']).to be_a(Hash)
   end
 
   it 'should report vhost' do
-    expect(magento.keys.join).to eql('magento.example.com')
+    expect(magento.keys[1]).to eql('magento.example.com')
   end
 
-  it "should report path" do
-    expect(magento['magento.example.com']['path']).to eql('/srv/magento/app/Mage.php')
+  it 'should report path' do
+    expect(magento['magento.example.com']['path']).to eql(
+      '/srv/magento/app/Mage.php')
   end
 
-  it "should report version" do
+  it 'should report version' do
     expect(magento['magento.example.com']['version']).to be_a(String)
   end
 
 end
-
